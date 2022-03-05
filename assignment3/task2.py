d996d265233b8b44c3528023b5130fe19408f680
Sander Aakerholt <sandera@oppdal.idi.ntnu.no>
2022-03-05 18:50:46 +0100

    Finilized models, need to get plots etc

--- assignment3/task2.py
+++ assignment3/task2.py
@@ -109,24 +109,24 @@ class TuningModel(nn.Module):
             nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=32,
-                out_channels=num_filters,
-                kernel_size=5,
-                stride=1,
-                padding=2
-            ),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Conv2d(
-                in_channels=32,
                 out_channels=32,
-                kernel_size=3,
+                kernel_size=5,
                 stride=1,
-                padding=1
+                padding=2
             ),
             nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
             nn.Conv2d(
                 in_channels=32,
-                out_channels=128,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
                 kernel_size=5,
                 stride=1,
                 padding=2
@@ -134,7 +134,7 @@ class TuningModel(nn.Module):
             nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
             nn.Conv2d(
-                in_channels=128,
+                in_channels=64,
                 out_channels=128,
                 kernel_size=3,
                 stride=1,
@@ -143,26 +143,29 @@ class TuningModel(nn.Module):
             nn.LeakyReLU(),
             nn.Conv2d(
                 in_channels=128,
-                out_channels=256,
+                out_channels=128,
                 kernel_size=5,
                 stride=1,
                 padding=2
             ),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.MaxPool2d(kernel_size=2,stride=2)
         )
         # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
-        self.num_output_features = 4*4*256
+        self.num_output_features = 4*4*128
         # Initialize our last fully connected layer
         # Inputs all extracted features from the convolutional layers
         # Outputs num_classes predictions, 1 for each class.
         # There is no need for softmax activation function, as this is
         # included with nn.CrossEntropyLoss
         self.classifier = nn.Sequential(
+            nn.Flatten(),
             nn.BatchNorm1d(self.num_output_features),
-            nn.Linear(self.num_output_features, 64),
+            nn.Linear(self.num_output_features, 1024),
             nn.LeakyReLU(),
-            nn.Linear(64, num_classes)
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
@@ -264,10 +267,13 @@ class TuningModel_2(nn.Module):
         # There is no need for softmax activation function, as this is
         # included with nn.CrossEntropyLoss
         self.classifier = nn.Sequential(
-            nn.BatchNorm1d(self.num_output_features),
-            nn.Linear(self.num_output_features, 64),
+            #nn.BatchNorm1d(self.num_output_features),
+            nn.Flatten(),
+            nn.Linear(self.num_output_features, 1024),
             nn.LeakyReLU(),
-            nn.Linear(64, num_classes)
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
@@ -288,7 +294,136 @@ class TuningModel_2(nn.Module):
         assert out.shape == (batch_size, self.num_classes),\
             f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
         return out
-    
+
+
+class TuningModel_3(nn.Module):
+
+    def __init__(self,
+                 image_channels,
+                 num_classes):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (3)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        # TODO: Implement this function (Task  2a)
+        num_filters = 32  # Set number of filters in first conv layer
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(
+                in_channels=image_channels,
+                out_channels=num_filters,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=512,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=512,
+                out_channels=512,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.BatchNorm2d(512),
+        )
+        # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
+        self.num_output_features = 2*2*512
+        # Initialize our last fully connected layer
+        # Inputs all extracted features from the convolutional layers
+        # Outputs num_classes predictions, 1 for each class.
+        # There is no need for softmax activation function, as this is
+        # included with nn.CrossEntropyLoss
+        self.classifier = nn.Sequential(
+            #nn.BatchNorm1d(self.num_output_features),
+            nn.Flatten(),
+            nn.Linear(self.num_output_features, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+        # TODO: Implement this function (Task  2a)
+        batch_size = x.shape[0]
+        #out = x    # what was given
+
+        # Copied from jupyter notebook, TODO: should possibly change it
+        out = self.feature_extractor(x)
+        out = out.view(batch_size, -1)
+        out = self.classifier(out) 
+        expected_shape = (batch_size, self.num_classes)
+        assert out.shape == (batch_size, self.num_classes),\
+            f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+        return out
+
 def create_plots(trainer: Trainer, name: str):
     plot_path = pathlib.Path("plots")
     plot_path.mkdir(exist_ok=True)
@@ -313,7 +448,7 @@ def main():
     utils.set_seed(0)
     epochs = 10
     batch_size = 64
-    learning_rate = 1e-2
+    learning_rate = 2e-2
     early_stop_count = 4
     dataloaders = load_cifar10(batch_size)
     model = TuningModel(image_channels=3, num_classes=10)
@@ -335,6 +470,7 @@ def main():
     test_loss,test_accuracy = compute_loss_and_accuracy(test_set, model, nn.CrossEntropyLoss())
     
     print(f"Train accuracy: {train_accuracy}\nValidation accuracy: {validation_accuracy}\nTest accuracy: {test_accuracy}\n")
+    print(f"Train loss: {train_loss}\nValidation loss: {validation_loss}\nTest loss: {test_loss}\n")
     create_plots(trainer, "task2")
 
 if __name__ == "__main__":
